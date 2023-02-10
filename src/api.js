d6ece8c165f6c42f7bea73218273915a85fdb278
Nikhilkumarb88 <nikhilkumarb88@gmail.com>
2023-02-10 20:16:48 +0530

    Weather-App

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "7f671e53b7msh1fe6ee3a29124bbp16f9a5jsnf3bbdf6a753d",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "d4d4dc27779373872775bcb62b8a5ee6";
